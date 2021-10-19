10fdf1cbb7691971d6ea3ee6be75f462ef6b6f30
Xiaocong Zhang <1627959262@qq.com>
2021-10-20 00:22:27 +0800

    pass Lab: Multithreading, didn't bother adding time.txt

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -22,15 +22,27 @@ barrier_init(void)
   bstate.nthread = 0;
 }
 
-static void 
+static void
 barrier()
 {
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  if(bstate.nthread<nthread-1){
+    bstate.nthread++;
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    pthread_mutex_unlock(&bstate.barrier_mutex);
+  }
+  else{
+    bstate.round++;
+    bstate.nthread = 0;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+    pthread_mutex_unlock(&bstate.barrier_mutex);
+  }
   // YOUR CODE HERE
   //
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+
 }
 
 static void *
